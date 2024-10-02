3bebd1ae39cb6ba2b8ae06a570d1972f7be3c018
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2024-10-02 20:41:15 +0530

    feat: add icons for options

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import {
     SelectTrigger,
     SelectContent,
     SelectItem,
-    SelectValue,
 } from "@/components/ui/select";
 import { Search } from "lucide-react";
 
 type FileType = {
     fileType: string;
     buttonData: string;
-    glyph: string;
+    imageUrl: string;
     placeholder: string;
 };
 
@@ -19,50 +18,50 @@ const fileTypes: FileType[] = [
     {
         fileType: "",
         buttonData: "Any",
-        glyph: "asterisk",
+        imageUrl: "/any.png",
         placeholder: "Search anything",
     },
     {
         fileType: "mkv|mp4|avi|mov|mpg|wmv|divx|mpeg",
         buttonData: "TV/Movies",
-        glyph: "film",
+        imageUrl: "/film.png",
         placeholder: "eg. The.Blacklist.S01",
     },
     {
         fileType:
             "MOBI|CBZ|CBR|CBC|CHM|EPUB|FB2|LIT|LRF|ODT|PDF|PRC|PDB|PML|RB|RTF|TCR|DOC|DOCX",
         buttonData: "Books",
-        glyph: "book",
+        imageUrl: "/book.png",
         placeholder: "eg. 1984",
     },
     {
         fileType: "mp3|wav|ac3|ogg|flac|wma|m4a|aac|mod",
         buttonData: "Music",
-        glyph: "music",
+        imageUrl: "/music.png",
         placeholder: "eg. K.Flay discography",
     },
     {
         fileType: "exe|iso|dmg|tar|7z|bz2|gz|rar|zip|apk",
         buttonData: "Software/ISO/DMG/Games",
-        glyph: "cd",
+        imageUrl: "/disc.png",
         placeholder: "eg. GTA V",
     },
     {
         fileType: "jpg|png|bmp|gif|tif|tiff|psd",
         buttonData: "Images",
-        glyph: "picture",
+        imageUrl: "/image.png",
         placeholder: "eg. Nature landscapes",
     },
 ];
 
 const searchEngines = ["google", "startpage", "filepursuit"] as const;
 const filePursuitTypes = [
-    "all",
-    "ebook",
-    "video",
-    "audio",
-    "mobile",
-    "archive",
+    { type: "all", imageUrl: "/any.png" },
+    { type: "ebook", imageUrl: "/book.png" },
+    { type: "video", imageUrl: "/film.png" },
+    { type: "audio", imageUrl: "/music.png" },
+    { type: "mobile", imageUrl: "/apk.png" },
+    { type: "archive", imageUrl: "/archive.png" },
 ];
 
 const SearchTool: React.FC = () => {
@@ -106,7 +105,7 @@ const SearchTool: React.FC = () => {
             )}`,
             filepursuit: `https://filepursuit.com/pursuit?q=${encodeURIComponent(
                 query
-            )}&type=${filePursuitType}&sort=datedesc`,
+            )}&type=${filePursuitType.type}&sort=datedesc`,
         };
 
         window.open(searchUrls[engine as keyof typeof searchUrls], "_blank");
@@ -114,13 +113,13 @@ const SearchTool: React.FC = () => {
 
     return (
         <div className="w-full min-h-screen flex flex-col justify-center items-center bg-white p-4 gap-4 md:gap-6">
-            <div className="flex items-center gap-2 md:gap-6 -mt-36">
+            <div className="flex items-center gap-2 -mt-36">
                 <img
                     src="logo.png"
                     alt="open index logo"
                     className="size-16 md:size-24"
                 />
-                <h1 className="text-4xl md:text-6xl font-exo text-stone-500">
+                <h1 className="text-4xl md:text-4xl font-exo text-stone-500">
                     Open Index
                 </h1>
             </div>
@@ -133,12 +132,25 @@ const SearchTool: React.FC = () => {
                     }
                 >
                     <SelectTrigger className="w-20 md:w-fit border-0 rounded-none focus:ring-0">
-                        <SelectValue placeholder="Select a search engine" />
+                        <div className="flex justify-center items-center">
+                            <img
+                                src={`/${engine}.png`}
+                                alt={engine}
+                                className="size-5"
+                            />
+                        </div>
                     </SelectTrigger>
                     <SelectContent>
                         {searchEngines.map((eng, index) => (
                             <SelectItem key={index} value={eng}>
-                                {eng.charAt(0).toUpperCase() + eng.slice(1)}
+                                <span className="flex items-center">
+                                    <img
+                                        src={`/${eng}.png`}
+                                        alt={eng}
+                                        className="size-5 mr-2"
+                                    />
+                                    {eng.charAt(0).toUpperCase() + eng.slice(1)}
+                                </span>
                             </SelectItem>
                         ))}
                     </SelectContent>
@@ -146,17 +158,34 @@ const SearchTool: React.FC = () => {
 
                 {engine === "filepursuit" && (
                     <Select
-                        value={filePursuitType}
-                        onValueChange={setFilePursuitType}
+                        value={filePursuitType.type}
+                        onValueChange={(value) =>
+                            setFilePursuitType(
+                                filePursuitTypes.find(
+                                    (ft) => ft.type === value
+                                ) || filePursuitTypes[0]
+                            )
+                        }
                     >
                         <SelectTrigger className="w-20 md:w-fit border-0 rounded-none focus:ring-0">
-                            <SelectValue placeholder="Select a file type" />
+                            <img
+                                src={filePursuitType.imageUrl}
+                                alt={filePursuitType.type}
+                                className="size-5"
+                            />
                         </SelectTrigger>
                         <SelectContent>
                             {filePursuitTypes.map((type, index) => (
-                                <SelectItem key={index} value={type}>
-                                    {type.charAt(0).toUpperCase() +
-                                        type.slice(1)}
+                                <SelectItem key={index} value={type.type}>
+                                    <span className="flex items-center gap-1">
+                                        <img
+                                            src={type.imageUrl}
+                                            alt={type.type}
+                                            className="size-5 mr-2"
+                                        />
+                                        {type.type.charAt(0).toUpperCase() +
+                                            type.type.slice(1)}
+                                    </span>
                                 </SelectItem>
                             ))}
                         </SelectContent>
@@ -171,7 +200,11 @@ const SearchTool: React.FC = () => {
                         }
                     >
                         <SelectTrigger className="w-20 md:w-fit border-0 rounded-none focus:ring-0">
-                            <SelectValue placeholder="Select a file type" />
+                            <img
+                                src={fileType.imageUrl}
+                                alt={fileType.buttonData}
+                                className="size-5"
+                            />
                         </SelectTrigger>
                         <SelectContent>
                             {fileTypes.map((type, index) => (
@@ -179,7 +212,14 @@ const SearchTool: React.FC = () => {
                                     key={index}
                                     value={index.toString()}
                                 >
-                                    {type.buttonData}
+                                    <span className="flex items-center gap-1">
+                                        <img
+                                            src={type.imageUrl}
+                                            alt={type.buttonData}
+                                            className="size-5 mr-2"
+                                        />
+                                        {type.buttonData}
+                                    </span>
                                 </SelectItem>
                             ))}
                         </SelectContent>
@@ -203,6 +243,13 @@ const SearchTool: React.FC = () => {
                     </div>
                 </div>
             </div>
+
+            {/* <button
+                className="px-10 py-4 rounded-lg bg-emerald-600 text-white text-lg"
+                onClick={startSearch}
+            >
+                Search
+            </button> */}
         </div>
     );
 };
